1a4e7b3cc4ffd78e4a167003d770f016453b9d2d
David Jangdal <d.jangdal@gmail.com>
2013-12-13 12:38:44 -0500

    Two scenes with the game scene having a square moving around

---
+++ BattleAntz/Assets/Scripts/Ant.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ant : MonoBehaviour {
+	private float delta_x = 0.3f;
+	private float delta_y = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Translate(delta_x*Random.Range(-1.0f,1.0f), delta_y*Random.Range(-1.0f,1.0f), 0);
+	}
+}
