e17914eb1850e95beaef75bdb686bc13a3eb0851
taintedcheese <heinmr@rose-hulman.edu>
2013-12-20 11:31:27 -0500

    Updated Hive class including constructor
    This is just for Mason because he loves to see text in the description
    box.

--- BattleAntz/Assets/Scripts/Hive.cs
+++ BattleAntz/Assets/Scripts/Hive.cs
@@ -2,16 +2,23 @@
 using System.Collections;
 
 public class Hive : MonoBehaviour {
-	int WORKER_COST = 10;
-	int WORKER_PRODUCTION = 5;
-	int ANT_COST = 10;
+	int WORKER_COST = 50;
+	int WORKER_PRODUCTION = 10;
+	int ARMY_ANT_COST = 50;
+	int BULL_ANT_COST = 100;
+	int FIRE_ANT_COST = 150;
+	int ANT_COST = 10; // ONLY FOR DEBUGGING PURPOSES
+
 	public int sugar;
 	public int health;
 	private int workers;
 	int income; // 10 Sugar per second base production
+	public Hive(){ // Constructor
+		Start ();
+	}
 	// Use this for initialization
 	void Start () {
-		income  = 1;
+		income  = 10;
 		sugar = 100;
 		health = 100;
 	}
@@ -23,16 +30,29 @@ public class Hive : MonoBehaviour {
 	
 	// Buy a certain number of workers.
 	void buyWorker(int n){
-		sugar -= n * WORKER_COST;
-		workers += n;
+		if(sugar > n*WORKER_COST){
+			sugar -= n * WORKER_COST;
+			workers += n;
+		}
 	}
 	
 	void buyAnt(int n){
-		sugar -= n * ANT_COST;
-		// SPAWN N ants
+		if (sugar > n * ANT_COST) {
+			sugar -= n * ANT_COST;
+			// TODO: SPAWN N ants
+		}
 	}
 	
 	void takeDamage(int damage){
 		health -= damage;
+		if (health <= 0) {
+			// TODO: end game
+		}
+	}
+
+	void upgrade(string upgrade){
+		// worker speed, armyant health, bullant strength, fireant special
+		//string ant = upgrade.Split[0]
+		//string cate = upgrade.Split[1]
 	}
 }
