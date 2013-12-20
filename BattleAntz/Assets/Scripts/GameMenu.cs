9347d5539297214c7aac2a4c4acd6421a1166918
Mason Schneider <schneimd@rose-hulman.edu>
2013-12-20 11:39:53 -0500

    Added Sugar and Health display

--- BattleAntz/Assets/Scripts/GameMenu.cs
+++ BattleAntz/Assets/Scripts/GameMenu.cs
@@ -9,20 +9,20 @@ public class GameMenu : MonoBehaviour {
 	private Rect antRect;
 
 	private AntController antController;
+	private Hive homeHive;
 	
 	// Use this for initialization
 	void Start () {
+		homeHive = GameObject.Find("Hive").GetComponent("Hive") as Hive;
 		pauseRect = new Rect(Screen.width/2 - 24, 24, 52, 55);
 		antRect = new Rect(Screen.width - 78*4, 20, 78, 58);
 		antController = GameObject.Find("Ant Controller").GetComponent("AntController") as AntController;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 	void OnGUI () {
 		GUI.Box(new Rect(0,0,Screen.width,90), "Battle Antz");
+		GUI.Label(new Rect(6,5,100,40), "Sugar: " + homeHive.sugar);
+		GUI.Label(new Rect(5,45,100,40), "Health: " + homeHive.health);
 		if (GuiButton.textureButton(pauseRect, pauseTexture)) {
 			Application.LoadLevel("MainMenu");
 		}
