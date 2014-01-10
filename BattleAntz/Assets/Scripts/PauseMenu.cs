d81abdd4e42abafff7ca6f0b19dc8d2621f5655f
David Jangdal <d.jangdal@gmail.com>
2014-01-10 12:13:43 -0500

    resumed timescale after restart

--- BattleAntz/Assets/Scripts/PauseMenu.cs
+++ BattleAntz/Assets/Scripts/PauseMenu.cs
@@ -21,10 +21,12 @@ public class PauseMenu : MonoBehaviour {
 		}
 		if(GUI.Button(new Rect(Screen.width/2-150,Screen.height/2-75,300,50),"Surrender")) {
 			Application.LoadLevel("MainMenu");
+			Time.timeScale = 1;
 		}
 		
 		if(GUI.Button(new Rect(Screen.width/2-150,Screen.height/2,300,50),"Restart")) {
 			Application.LoadLevel("GameScene");
+			Time.timeScale = 1;
 		}
 	}
 
