2fb4b2c3c4fd1ad22e397c5785c951ef38451c26
Mason Schneider <schneimd@rose-hulman.edu>
2014-01-11 23:19:52 -0500

    Added EndGame as well as some folder orginization

---
+++ BattleAntz/Assets/Scripts/EndScene/EndGameStats.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGameStats : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI () {
+		GUI.Box(new Rect(Screen.width/2-200,Screen.height/2-300,400,600),"Game Over!");
+
+		//TODO: Display end game stats
+
+		if(GUI.Button(new Rect(Screen.width/2-150,Screen.height/2+100,300,50),"Main Menu")) {
+			Application.LoadLevel("MainMenu");
+			Time.timeScale = 1;
+		}
+		
+		if(GUI.Button(new Rect(Screen.width/2-150,Screen.height/2+200,300,50),"Play Again")) {
+			Application.LoadLevel("GameScene");
+			Time.timeScale = 1;
+		}
+	}
+}
