c0d5e0046a39b8ee6df7d8f248cff42578b2159f
David Jangdal <d.jangdal@gmail.com>
2014-01-14 11:50:58 -0500

    added result label

--- BattleAntz/Assets/Scripts/GameOverMenu.cs
+++ BattleAntz/Assets/Scripts/GameOverMenu.cs
@@ -2,10 +2,13 @@
 using System.Collections;
 
 public class GameOverMenu : MonoBehaviour {
-	string gameResult;
+	private string gameResult;
+	private GUIStyle style = new GUIStyle();
+
 	// Use this for initialization
 	void Start () {
 		enabled = false;
+		style.fontSize = 30;
 	}
 	
 	// Update is called once per frame
@@ -20,7 +23,9 @@ public class GameOverMenu : MonoBehaviour {
 	}
 	
 	void OnGUI () {
-		GUI.Box(new Rect(Screen.width/2-200,Screen.height/2-200,400,300), gameResult);
+		GUI.Box(new Rect(Screen.width/2-200,Screen.height/2-200,400,300), "Game Ended");
+		GUI.TextField(new Rect(Screen.width/2-60,Screen.height/2-150,300,50), gameResult, style);
+
 		if(GUI.Button(new Rect(Screen.width/2-150,Screen.height/2-75,300,50),"Main Menu")) {
 			Application.LoadLevel("MainMenu");
 			Time.timeScale = 1;
