c802e3b186ca615d40517a1d22b5507365b95c49
Mason Schneider <schneimd@rose-hulman.edu>
2014-01-17 14:03:09 -0500

    Added upgrade menu, still need to do upgrade calls.

--- BattleAntz/Assets/Scripts/GameScene/GameMenu.cs
+++ BattleAntz/Assets/Scripts/GameScene/GameMenu.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class GameMenu : MonoBehaviour {
@@ -10,24 +10,34 @@ public class GameMenu : MonoBehaviour {
 	private Rect pauseRect;
 
 	public Hive playerHive;
+
+	private UpgradeMenu upgrades;
 	
 	// Use this for initialization
 	void Start () {
 		gameObject.GetComponent<PauseMenu>().enabled = false;
+		upgrades = gameObject.GetComponent<UpgradeMenu>();
+		upgrades.enabled = false;
 		pauseRect = new Rect(Screen.width/2 - 24, 24, 0, 0);
 	}
 
 	void OnGUI () {
+		// Create the menu area
 		GUI.Box(new Rect(0,0,Screen.width,90), "Battle Antz");
+
+		// Display hive stats
 		GUI.Label(new Rect(16,15,100,40), "Sugar: " + playerHive.sugar);
 		GUI.Label(new Rect(15,55,100,40), "Health: " + playerHive.health + "/100");
 		GUI.Label(new Rect(140,15,100,40), "Sugar/Sec: " + playerHive.income);
 		GUI.Label(new Rect(140,55,100,40), "Workers: " + playerHive.workers);
+
+		// Pause Game
 		if (GuiButton.textureButton(pauseRect, pauseTexture)) {
 			Time.timeScale = 0;
 			gameObject.GetComponent<PauseMenu>().enabled = true;
 		}
 
+		// Manage Workers
 		if(GUI.Button(new Rect(215,55,43,20),"Buy")) {
 			playerHive.buyWorker();
 		}
@@ -35,17 +45,33 @@ public class GameMenu : MonoBehaviour {
 		if(GUI.Button(new Rect(260,55,43,20),"Sell")) {
 			playerHive.sellWorker();
 		}
-		
-		if (GuiButton.textureButton(new Rect(Screen.width - 100*3 - 100, 10, 0, 0), armyAntTexture)) {
+		// --------------
+
+		// Manage Army Ants
+		if (GUI.Button(new Rect(Screen.width - 350, 35, 50, 50), "Army")) {
 			playerHive.buyArmyAnt();
 		}
-		
-		if (GuiButton.textureButton(new Rect(Screen.width - 100*2 - 50, 10, 0, 0), fireAntTexture)) {
+		if (GUI.Button(new Rect(Screen.width - 338, 10, 25, 25), "+")) {
+			upgrades.type = "army";
+			upgrades.enabled = true;
+		}
+
+		// Manage Fire Ant
+		if (GUI.Button(new Rect(Screen.width - 250, 35, 50, 50), "Fire")) {
 			playerHive.buyFireAnt();
 		}
-		
-		if (GuiButton.textureButton(new Rect(Screen.width - 100*1, 10, 0, 0), bullAntTexture)) {
+		if (GUI.Button(new Rect(Screen.width - 238, 10, 25, 25), "+")) {
+			upgrades.type = "fire";
+			upgrades.enabled = true;
+		}
+
+		// Manage Bull Ant
+		if (GUI.Button(new Rect(Screen.width - 150, 35, 50, 50), "Bull")) {
 			playerHive.buyBullAnt();
+		}
+		if (GUI.Button(new Rect(Screen.width - 138, 10, 25, 25), "+")) {
+			upgrades.type = "bull";
+			upgrades.enabled = true;
 		}
 	}
 }
---
+++ BattleAntz/Assets/Scripts/GameScene/UpgradeMenu.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpgradeMenu : MonoBehaviour {
+
+	public string type;
+
+	// Use this for initialization
+	void Start () {
+		type = "army";
+	}
+
+	void OnGUI() {
+		GUI.Box(new Rect(Screen.width-400,90,400,200), "Upgrades");
+		if(GUI.Button(new Rect(Screen.width-30,95,25,25), "X")) {
+			this.enabled = false;
+		}
+		int typeInt = 0;
+		int upgrade = 0;
+		switch(type) {
+		case "army":
+			typeInt = 1;
+			break;
+		case "fire":
+			typeInt = 2;
+			break;
+		case "bull":
+			typeInt = 3;
+			break;
+		default:
+			typeInt = 0;
+			break;
+		}
+
+		showInfo();
+
+		if(GUI.Button(new Rect(Screen.width-375,120,100,30),"Speed")) {
+			upgrade = 0;
+		}
+		if(GUI.Button(new Rect(Screen.width-375,160,100,30),"Health")) {
+			upgrade = 1;
+		}
+		if(GUI.Button(new Rect(Screen.width-375,200,100,30),"Strength")) {
+			upgrade = 2;
+		}
+		if(GUI.Button(new Rect(Screen.width-375,240,100,30),"Special")) {
+			upgrade =3;
+		}
+
+
+	}
+
+	private void showInfo() {
+		string desc = "";
+		switch(type) {
+		case "army":
+			desc = "Army ants are standard fighters used to wage war against the rival colony. These ants move at a normal speed and have normal health. Army ants are highly effective against  Bull Ants because they can swarm and overpower them.";
+			break;
+		case "fire":
+			desc = "Fire ants are small insects with a very strong attack. What they lack in health they make up for in damage. These ants are very effective against Army Ants due to their range and ability to bring the pain.";
+			break;
+		case "bull":
+			desc = "Bull ants are massive insects that can withstand a large amount of damage but struggle to deal much damage of their own. These ants are very effective against Fire Ants because they can crush those puny punks and live through the pain.";
+			break;
+		default:
+			desc = "";
+			break;
+		}
+		GUI.Label(new Rect(Screen.width-250,130,200,150), desc);
+	}
+
+	private void showSpeed() {
+
+	}
+}
