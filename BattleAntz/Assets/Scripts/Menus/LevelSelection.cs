5c6cf057f56a09893af54e95b1e53d9b5e6c7986
RHITlim <maderli@rose-hulman.edu>
2014-02-06 11:54:07 -0500

    Added Final Level Button

--- BattleAntz/Assets/Scripts/Menus/LevelSelection.cs
+++ BattleAntz/Assets/Scripts/Menus/LevelSelection.cs
@@ -18,43 +18,18 @@ public class LevelSelection : MonoBehaviour {
 //	int row = 0;
 
 	void OnGUI () {
-		//TODO Maybe do this with for loop and space out the buttons. 
 		for(int i=0; i<maxlevels; i++){
 			if (GUI.Button(new Rect(100+spacingX*(i%3), 100+spacingY*(i/3), 200, 50), "Level " + (i+1))) {
 				Application.LoadLevel("GameScene");
 				Constants.level = i;
 			}
 		}
-//		if (GUI.Button(new Rect(100, 100, 200, 50), "Level 1")) {
-//			Application.LoadLevel("GameScene");
-//		}
-//		if (GUI.Button(new Rect(400, 100, 200, 50), "Level 2")) {
-//			Application.LoadLevel("GameScene");
-//		}
-//		if (GUI.Button(new Rect(700, 100, 200, 50), "Level 3")) {
-//			Application.LoadLevel("GameScene");
-//		}
-//		if (GUI.Button(new Rect(100, 250, 200, 50), "Level 4")) {
-//			Application.LoadLevel("GameScene");
-//		}
-//		if (GUI.Button(new Rect(400, 250, 200, 50), "Level 5")) {
-//			Application.LoadLevel("GameScene");
-//		}
-//		if (GUI.Button(new Rect(700, 250, 200, 50), "Level 6")) {
-//			Application.LoadLevel("GameScene");
-//		}
-//		if (GUI.Button(new Rect(100, 400, 200, 50), "Level 7")) {
-//			Application.LoadLevel("GameScene");
-//		}
-//		if (GUI.Button(new Rect(400, 400, 200, 50), "Level 8")) {
-//			Application.LoadLevel("GameScene");
-//		}
-//		if (GUI.Button(new Rect(700, 400, 200, 50), "Level 9")) {
-//			Application.LoadLevel("GameScene");
-//		}
-//		if (GUI.Button(new Rect(400, 550, 200, 50), "Final Level")) {
-//			Application.LoadLevel("GameScene");
-//		}
+
+		//This level will load an AI to react to the user instead of a scheduled order of what units the AI builds
+		if (GUI.Button (new Rect (400, 550, 200, 50), "Final Level")) {
+			Application.LoadLevel ("GameScene");
+			Constants.level = 10;
+		}
 
 		//Back button
 		if (GUI.Button(new Rect(800, 700, 150, 50), "Back")){
